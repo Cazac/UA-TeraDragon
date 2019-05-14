d6d612f23926ade05621375ea5e30b067e006746
Cazac <cazac1350@gmail.com>
2019-05-13 17:02:29 -0700

    Main Menu 1st Pass
    - Imported Textmesh Pro
    - Added main menu basics
    - Created ButtonController script

---
+++ Assets/Scripts/TD_ButtonController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TD_ButtonController : MonoBehaviour
+{
+
+    ///////////////
+    /// <summary>
+    ///     
+    /// TD_ButtonController is used to manage all of the button inputs
+    /// 
+    /// </summary>
+    ///////////////
+
+    ////////////////////////////////
+
+    private bool isMusicPlaying;
+    private bool isSFXPlaying; 
+
+    ///////////////////////////////////////////////////////////////// - Main Menu
+
+    private void Button_Play()
+    {
+
+    }
+
+    private void Button_Help()
+    {
+
+    }
+
+    private void Button_Credits()
+    {
+
+    }
+
+    private void Button_Quit()
+    {
+
+    }
+
+    ///////////////////////////////////////////////////////////////// - Audio Menu
+
+    private void Button_Music()
+    {
+
+    }
+
+    private void Button_SFX()
+    {
+
+    }
+
+    /////////////////////////////////////////////////////////////////
+}
