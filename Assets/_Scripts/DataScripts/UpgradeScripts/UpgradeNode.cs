5d267734a4360af58bbe8c9b8d2a81812a9ec3da
Pierre Cudennec <pierre.yves.cudennec@gmail.com>
2019-05-22 08:50:51 -0400

    misc update

--- Assets/_Scripts/DataScripts/UpgradeScripts/UpgradeNode.cs
+++ Assets/_Scripts/DataScripts/UpgradeScripts/UpgradeNode.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class UpgradeNode : ScriptableObject {
+public abstract class UpgradeNode : ScriptableObject {
     public string upgradeName;
     private UpgradeType upgradeType;
     public bool isActive;
