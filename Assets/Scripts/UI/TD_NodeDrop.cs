7a8452dbec48fadd27a7eb2d6cad72dccda0366f
Zac <cazac1350@gmail.com>
2019-05-30 19:41:13 -0700

    Prefab Crash Test

--- Assets/Scripts/UI/TD_NodeDrop.cs
+++ Assets/Scripts/UI/TD_NodeDrop.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TD_NodeDrop : MonoBehaviour
+public class TD_NodeDrop : MonoBehaviour, IDropHandler, IDragHandler
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +16,26 @@ public class TD_NodeDrop : MonoBehaviour
     {
         
     }
+
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+    
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+        print("Add");
+
+        //Check Money
+        //Charge PLyaer
+        //Spawn Tower UI
+        //Get Reff To Tower
+        //Attach to Cursor
+        //
+
+    }
+
 }
--- Assets/Scripts/UI/TD_TowerDrag.cs
+++ Assets/Scripts/UI/TD_TowerDrag.cs
@@ -1,19 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TD_TowerDrag : MonoBehaviour, IDragHandler, IEndDragHandler
+public class TD_TowerDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     ///////////////
     /// <summary>
     ///     
-    /// TD_TowerDrag is used to drag and drop all towers into the game
+    /// TD_TowerDrag is used to drag and drop all towers into the game onto TD_TowerDrop sockets
     /// 
     /// </summary>
     ///////////////
 
+    public GameObject towerPrefab;
+    public GameObject currentTower;
+
+    public GameObject cursor;
+
     ////////////////////////////////
+
+
+    public int tileLayer;
+
+    private WaveManager waveManager;
+
+    private TD_TileNodes tD_TileNodes;
 
 
     /////////////////////////////////////////////////////////////////
@@ -21,7 +34,7 @@ public class TD_TowerDrag : MonoBehaviour, IDragHandler, IEndDragHandler
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -32,8 +45,22 @@ public class TD_TowerDrag : MonoBehaviour, IDragHandler, IEndDragHandler
 
     /////////////////////////////////////////////////////////////////
 
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        print("Thing");
+
+        currentTower = Instantiate(towerPrefab);
+
+
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
+        //print("THOG");
+
+
+        currentTower.transform.position = cursor.transform.position;
+
 
 
         //Check Money
@@ -47,6 +74,41 @@ public class TD_TowerDrag : MonoBehaviour, IDragHandler, IEndDragHandler
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        print("Destroy");
+        Destroy(currentTower);
+
+
+
+
+
+        /// <summary>
+        ///Detects mouse click and performs raycast to a tile, if detected then turn that tile to the color black
+        /// </summary>
+        ///<para>
+        ///Uses raycast from mouse position to collider
+        ///</para>
+
+
+        return;
+        
+
+        Ray raycastMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(raycastMouse, out hit, Mathf.Infinity, tileLayer))
+        {
+            //TODO: Color is hardcoded to black when a tile is clicked, need to change to dynamic
+            hit.collider.gameObject.GetComponent<SpriteRenderer>().color = Color.black;
+            LogRaycasthitObject(hit.collider.gameObject.transform.position.ToString(),
+            hit.collider.gameObject.transform.parent.gameObject.name);
+
+            //Store hit tile node in a list in tD_TileNodes
+            tD_TileNodes.SelectedNode.Add(hit.collider.gameObject);
+
+        }
+
+
+     
 
         //Find Nearest Node
 
@@ -55,6 +117,16 @@ public class TD_TowerDrag : MonoBehaviour, IDragHandler, IEndDragHandler
 
         //if Node Call Tower Spawner on Node
 
+
+
+
+    }
+
+
+    internal void LogRaycasthitObject(String position, String type)
+    {
+        String logString = String.Format("Hit node at position: {0}, is type of: {1}", position, type);
+          Debug.Log(logString);
     }
 
     /////////////////////////////////////////////////////////////////
