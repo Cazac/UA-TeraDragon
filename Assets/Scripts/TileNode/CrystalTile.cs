83116b56177a7b4b99a9a5d95cc1b507899ec0c6
Pierre Cudennec <pierre.yves.cudennec@gmail.com>
2019-06-21 23:49:24 -0400

    added unique crystal color + fixed small bug in randpmized selector

--- Assets/Scripts/TileNode/CrystalTile.cs
+++ Assets/Scripts/TileNode/CrystalTile.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class CrystalTile : WorldTile
 {
-
+    [Range(10, 50)]
     public int crystalValueRed = 25;
+    [Range(10, 50)]
     public int crystalValueBlue = 25;
+    [Range(10, 50)]
     public int crystalValueGreen = 25;
+    [Range(10, 50)]
     public int crystalValueYellow = 25;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+    //    RandomCrystalValues();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    /// <summary>
+    /// Selects a color for the crystal. probabilities are determined by each Crystal values
+    /// </summary>
+    /// <returns>Color of the crystal</returns>
     public CrystalColor ProduceRandomCrystal()
     {
         int total = crystalValueRed + crystalValueBlue + crystalValueGreen + crystalValueYellow;
@@ -45,12 +44,42 @@ public class CrystalTile : WorldTile
         }
     }
 
+    /// <summary>
+    /// Generates random values for each crystal values
+    /// </summary>
     void RandomCrystalValues()
     {
         crystalValueRed = Random.Range(10, 51);
         crystalValueBlue = Random.Range(10, 51);
         crystalValueGreen = Random.Range(10, 51);
         crystalValueYellow = Random.Range(10, 51);
+    }
+
+    /// <summary>
+    /// Set crystal tile such a that only 1 color crystal is produced
+    /// </summary>
+    /// <param name="cc">Unique color chosen</param>
+    void UniqueCrystalValue(CrystalColor cc)
+    {
+        switch (cc)
+        {
+            case CrystalColor.RED:
+                crystalValueRed = 1;
+                crystalValueBlue = crystalValueGreen = crystalValueYellow = 0;
+                return;
+            case CrystalColor.YELLOW:
+                crystalValueYellow = 1;
+                crystalValueBlue = crystalValueGreen = crystalValueRed = 0;
+                return;
+            case CrystalColor.BLUE:
+                crystalValueBlue = 1;
+                crystalValueRed = crystalValueGreen = crystalValueYellow = 0;
+                return;
+            case CrystalColor.GREEN:
+                crystalValueGreen = 1;
+                crystalValueBlue = crystalValueRed = crystalValueYellow = 0;
+                return;
+        }
     }
 }
 
