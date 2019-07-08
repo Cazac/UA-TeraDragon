250c6fc7cb397ffd196468ecd1ce5e7d9562a73b
Mateusz Wojciechowski <mattchowski@gmail.com>
2019-07-08 11:33:43 +0200

    Made some modifications to the menu, nothing serious, just made a scriptable object so i can handle the colors and stuff. Working on it further

---
+++ Assets/Source/Menu Art/Scripts/ButtonsSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[CreateAssetMenu(fileName = "New Settings", menuName = "ButtonAssetSettings")]
+public class ButtonsSettings : ScriptableObject
+{
+    public Color32 normalColor;
+    public Color32 highlightColor;
+    public Color32 pressedColor;
+    public Color32 selectedColor;
+
+    public float colorMultiplier = 1;
+    public float transitionTime = 0.1f;
+    public FontStyles highlightFontStyle;
+
+
+}
---
+++ Assets/Source/Menu Art/Scripts/MenuButtonToggles.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using TMPro;
+
+public class MenuButtonToggles : Selectable, IPointerClickHandler
+{
+    private TextMeshProUGUI _targetGraphic;
+    private ColorBlock _myColor;
+
+    public UnityEvent onClick;
+    public ButtonsSettings customColorSettings;
+
+    void Start()
+    {
+        //set the target graphic automatically, has to be 2nd child 
+        _targetGraphic = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        this.targetGraphic = _targetGraphic;
+
+        //set the colors from the scriptable object
+        _myColor.normalColor = customColorSettings.normalColor;
+        _myColor.highlightedColor = customColorSettings.highlightColor;
+        _myColor.pressedColor = customColorSettings.pressedColor;
+        _myColor.selectedColor = customColorSettings.selectedColor;
+        _myColor.colorMultiplier = customColorSettings.colorMultiplier;
+        _myColor.fadeDuration = customColorSettings.transitionTime;
+
+        //finally set the colors to be used from the scriptable object
+        this.colors = _myColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        onClick.Invoke();
+    }
+}
