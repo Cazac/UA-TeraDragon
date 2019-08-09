4c0b4ce3b2773b41d4819954ea97f2766d105db1
Zac <cazac1350@gmail.com>
2019-08-08 20:49:52 -0700

    Credits Starting Point

---
+++ Assets/Scenes/CreditsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour
+{
+
+    public Animator CreditsAnimator;
+
+
+    void Awake()
+    {
+        FindObjectOfType<SoundManager>().PlaySpecificSound("Peace");
+    }
+
+    private void Start()
+    {
+
+        StartCoroutine(CreditsPlay());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            CreditsAnimator.speed = 3f;
+        }
+        else
+        {
+            CreditsAnimator.speed = 1;
+        }
+    }
+
+    private IEnumerator CreditsPlay()
+    {
+        yield return new WaitForSeconds(2.5f);
+
+
+
+
+
+        //Credit Slidshow
+        CreditsAnimator.Play("Play");
+    }
+
+}
